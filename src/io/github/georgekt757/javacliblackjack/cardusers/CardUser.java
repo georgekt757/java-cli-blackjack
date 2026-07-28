07b2ea97dbabe74c22628e633133cd5736b2774e
georgekt757 <161618232+georgekt757@users.noreply.github.com>
2026-07-28 15:19:09 +0100

    Player class done

--- src/io/github/georgekt757/javacliblackjack/cardusers/CardUser.java
+++ src/io/github/georgekt757/javacliblackjack/cardusers/CardUser.java
@@ -47,3 +47,39 @@ abstract class CardUser {
         this.hand.clear();
     }
 }
+
+class Player extends CardUser {
+    private final String name;
+    private int bet;
+    private int pot;
+    private final int initialPot;
+
+    public Player(String name, int bet, int pot) {
+        this.name = name;
+        this.bet = bet;
+        this.pot = pot;
+        this.initialPot = pot;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getBet() {
+        return this.bet;
+    }
+    public void setBet(int bet) {
+        this.bet = bet;
+    }
+
+    public int getPot() {
+        return this.pot;
+    }
+    public void setPot(int pot) {
+        this.pot = pot;
+    }
+
+    public int getInitialPot() {
+        return this.initialPot;
+    }
+}
