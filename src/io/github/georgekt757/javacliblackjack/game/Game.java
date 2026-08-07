985dd9401f5ec1c74da285c4c454ee695a9aaf4f
georgekt757 <161618232+georgekt757@users.noreply.github.com>
2026-08-07 16:31:36 +0100

    Separated game logic from Blackjack class to a new Game class

---
+++ src/io/github/georgekt757/javacliblackjack/game/Game.java
@@ -0,0 +1,75 @@
+package io.github.georgekt757.javacliblackjack.game;
+
+import io.github.georgekt757.javacliblackjack.cardusers.Dealer;
+import io.github.georgekt757.javacliblackjack.cardusers.Player;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+
+public class Game {
+    private Player player;
+    private Dealer dealer;
+    private Scanner kbd;
+
+    public Game() {
+        kbd = new Scanner(System.in);
+        player = initialisePlayer(kbd);
+        dealer = new Dealer();
+    }
+
+    public Player initialisePlayer(Scanner kbd) {
+        // This method creates the player through use of the Scanner object, with validation
+        // via try-catch blocks for the numerical values to ensure the program does not crash
+        String name;
+        int bet = 0, pot = 0;
+        final int MINIMUM = 10; // Change this value if you wish to alter the minimum starting value
+        boolean waiter = true;
+        
+        System.out.print("What is your name?: ");
+        name = kbd.nextLine();
+        
+        System.out.println("Next comes inputting how much money you have and are willing to gamble, and how much of it you are betting each round.\nBoth must be a multiple of five.");
+        while (waiter) {
+            try {
+                // Player inputs their pot here, and it is validated to ensure it is always an integer
+                // and is valid to the constraints given
+                System.out.print("How much money are you willing to gamble?: ");
+                pot = kbd.nextInt();
+
+                if (pot < MINIMUM) {
+                    System.out.println("You're have too little money!");
+                } else if (pot % 5 != 0) {
+                    System.out.println("The pot must be a multiple of five!");
+                } else {
+                    waiter = false;
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("You need to input a valid whole number!");
+            }
+        }
+
+        waiter = true;
+        while (waiter) {
+            try {
+                // Player inputs their bet here, and it is validated to ensure it is always an integer
+                // and valid to the constraints given
+                System.out.print("How much are you betting? (You can change your bet at any time.): ");
+                bet = kbd.nextInt();
+
+                if (bet > pot) {
+                    System.out.println("You're betting more money than you have!");
+                } else if (bet < 5) {
+                    System.out.println("Your bet is too low! It must be at least five!");
+                } else if (bet % 5 != 0) {
+                    System.out.println("Your bet must be a multiple of five!");
+                } else {
+                    waiter = false;
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("You need to input a valid whole number!");
+            }
+        }
+
+        return new Player(name, bet, pot);
+    }
+}
